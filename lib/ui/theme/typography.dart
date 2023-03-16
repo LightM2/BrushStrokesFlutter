dd32e67589b245d5d2acff333c11781984d6c3bb
Svitlana Mozharovska <svitlana.mozharovska@uptech.team>
2023-03-16 10:28:46 +0100

    feature(navigation_bar): fix typography

---
+++ lib/ui/theme/typography.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final textTheme = TextTheme(
+  displayLarge: GoogleFonts.notoSansLao(
+      fontSize: 96,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -1.5
+  ),
+  displayMedium: GoogleFonts.notoSansLao(
+      fontSize: 60,
+      fontWeight: FontWeight.w300,
+      letterSpacing: -0.5
+  ),
+  displaySmall: GoogleFonts.notoSansLao(
+      fontSize: 48,
+      fontWeight: FontWeight.w400
+  ),
+  headlineLarge: GoogleFonts.notoSansLao(
+      fontSize: 44,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25
+  ),
+  headlineMedium: GoogleFonts.notoSansLao(
+      fontSize: 34,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25
+  ),
+  headlineSmall: GoogleFonts.notoSansLao(
+      fontSize: 24,
+      fontWeight: FontWeight.w400
+  ),
+  titleLarge: GoogleFonts.notoSansLao(
+      fontSize: 20,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.15
+  ),
+  titleMedium: GoogleFonts.notoSansLao(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.15
+  ),
+  titleSmall: GoogleFonts.notoSansLao(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 0.1
+  ),
+  bodyLarge: GoogleFonts.notoSansLao(
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.5
+  ),
+  bodyMedium: GoogleFonts.notoSansLao(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.25
+  ),
+  bodySmall: GoogleFonts.notoSansLao(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 0.4
+  ),
+  labelLarge: GoogleFonts.notoSansLao(
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      letterSpacing: 1.25
+  ),
+  labelMedium: GoogleFonts.notoSansLao(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.25
+  ),
+  labelSmall: GoogleFonts.notoSansLao(
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      letterSpacing: 1.5
+  ),
+);
